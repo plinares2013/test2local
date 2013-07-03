349fbdaf578c2a732b45a0d9c713440c7227f3d5
Paul Linares <plinares2006@yahoo.com>
2013-07-03 15:27:57 -0700

    Diverigin more with branch test

--- README.txt
+++ README.txt
@@ -6,3 +6,5 @@ Adding this line on 64b machine.
 Adding the line with dimension5100
 
 "Test" branch has been created. We are diverging starting from this line.
+
+"Test" branch. Second line.
--- python.py
+++ python.py
@@ -4,3 +4,5 @@ print 'Added with 64 bit machine'
 print 'Added from dimension5100'
 
 print 'This is the first line in the test branch. We are now diverging.'
+
+print 'Second line with "test" branch."
